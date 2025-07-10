71a122ac7bfc7507482290855a724142c52c080d
MasyaSm <kuvalda-max@mail.ru>
2025-07-10 17:41:52 +0300

    hot fix provider

--- src/Laravel/Providers/FreedomBrokerServiceProvider.php
+++ src/Laravel/Providers/FreedomBrokerServiceProvider.php
@@ -2,45 +2,56 @@
 
 namespace MasyaSmv\FreedomBrokerApi\Laravel\Providers;
 
-use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
 use Illuminate\Support\ServiceProvider;
 use MasyaSmv\FreedomBrokerApi\Core\Http\FreedomHttpClient;
 use MasyaSmv\FreedomBrokerApi\Core\Http\Signer\V1Signer;
+use MasyaSmv\FreedomBrokerApi\Core\Http\Signer\V2Signer;
 use MasyaSmv\FreedomBrokerApi\Core\Parser\ReportParser;
-use MasyaSmv\FreedomBrokerApi\Core\Service\OrderService;
-use MasyaSmv\FreedomBrokerApi\Core\Service\QuoteInfoService;
 use MasyaSmv\FreedomBrokerApi\Core\Service\ReportService;
-use MasyaSmv\FreedomBrokerApi\Core\Service\StockHistoryService;
-use MasyaSmv\FreedomBrokerApi\Core\Service\StockService;
 use MasyaSmv\FreedomBrokerApi\Laravel\FreedomManager;
 
 class FreedomBrokerServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(FreedomHttpClient::class, function () {
+        // 1) Привязываем GuzzleHttp\ClientInterface → GuzzleHttp\Client
+        $this->app->bind(ClientInterface::class, function () {
+            return new Client([
+                'timeout' => config('freedom.timeout', 15),
+                'verify' => config('freedom.verify_ssl', false),
+            ]);
+        });
+
+        // 2) Привязываем FreedomHttpClient
+        $this->app->singleton(FreedomHttpClient::class, function ($app) {
+            $pub = config('freedom.public_key');
+            $priv = config('freedom.private_key');
+            $ver = config('freedom.version', FreedomHttpClient::V1);
+            $signer = $ver === FreedomHttpClient::V2
+                ? new V2Signer($priv)
+                : new V1Signer($priv);
+
             return new FreedomHttpClient(
-                http: Http::timeout(15)->buildClient(),
-                signer: new V1Signer(config('freedom.private_key')),
-                apiKey: config('freedom.public_key'),
-                version: FreedomHttpClient::V1,
+                http: $app->make(ClientInterface::class),
+                signer: $signer,
+                apiKey: $pub,
+                version: $ver,
+                apiUrl: config('freedom.api_url', 'https://tradernet.ru/api'),
             );
         });
 
-        $this->app->bind(ReportService::class, function ($app) {
+        // 3) Привязываем ReportService (он зависит от FreedomHttpClient и ReportParser)
+        $this->app->singleton(ReportService::class, function ($app) {
             return new ReportService(
                 $app->make(FreedomHttpClient::class),
                 new ReportParser(),
             );
         });
 
-        $this->app->bind(StockService::class);
-        $this->app->bind(QuoteInfoService::class);
-        $this->app->bind(StockHistoryService::class);
-
-        $this->app->bind(OrderService::class, fn ($app) => new OrderService($app->make(FreedomHttpClient::class)));
-
-        $this->app->singleton(ReportService::class, function ($app) {
+        // 4) Если вы сделали FreedomManager, то биндите его под фасад:
+        $this->app->singleton('freedom', function ($app) {
             return new FreedomManager();
         });
     }
